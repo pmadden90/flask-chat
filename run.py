304317e5495173e2a222508f0eca8f851512f4a3
Paul Madden <p.madden90@gmail.com>
2019-11-08 09:10:44 +0000

    Added Procfile for deployment

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, render_template, request, session, url_for
 
 
 app = Flask(__name__)
-app.secret_key = "randomstring123"
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
 
@@ -38,4 +38,4 @@ def user(username):
     return render_template("chat.html", username=username,
                            chat_messages=messages)
 
-app.run(os.getenv('IP'), port=int(os.getenv('PORT', '3000')), debug=True)
+app.run(os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=True)
